52b77f6c259b8fb8b40a6f48d50ae56ff28a9cc8
CharlotteSmoor <charlottesmoor@gmail.com>
2016-03-09 13:28:18 +0100

    path

--- src/com/company/Grid.java
+++ src/com/company/Grid.java
@@ -8,7 +8,7 @@ public class Grid {
     private int rows;
     private int columns;
     private int[][] grid;
-    private Search trial;
+    private String path;
 
 
     public Grid(int rows, int columns){
