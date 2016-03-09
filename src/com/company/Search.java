a67d9127f6482d34454cc8d18299eb403885b98b
sanneeggengoor <sanneeggengoor@gmail.com>
2016-03-09 12:38:42 +0100

    Druk bezig

---
+++ src/com/company/Search.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sanne on 9-3-2016.
+ */
+public class Search {
+    ArrayList<Car> carList;
+
+    public Search(){
+        ArrayList<Car> carList = new ArrayList<>(30);
+    }
+
+    public void addCarList(Car car4list){
+
+    }
+}
