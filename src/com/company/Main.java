138018c0f1d6c23149485414b35dd215d1bd17ed
sanneeggengoor <sanneeggengoor@gmail.com>
2016-03-24 13:19:03 +0100

    Merge branch 'master' of https://github.com/sanneeggengoor/datarushhour
    # Conflicts:
    #	src/com/company/Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //parse args
+        // for every grid (except the greatest) run the program
         for (int i = 1; i<5; i++) {
             int puzzlenum = i;
             new Main(puzzlenum).run();
         }
     }
+
     private int puzzlenum;
     public Search trial;
 
@@ -21,7 +22,7 @@ public class Main {
     public void run() {
         System.out.println(puzzlenum);
 
-        // create a trial, add first grid and search for solution
+        // create a trial, add grid corresponding to the puzzlenum and search for solution
         trial = new Search();
         Games game= new Games();
         trial.addNode(game.getGame(puzzlenum));
